9459372e4a9ffad28436d214851e5409222f230d
tofu-10 <rijanstha.lm10@gmail.com>
2025-12-16 14:21:30 +0545

    finished simple logic using filters, reduce and find

--- week2/DOMsimplelogicarrays.js
+++ week2/DOMsimplelogicarrays.js
@@ -1,4 +1,20 @@
 let nums = [1, 2, 3, 4, 5, 6];
-function evenSquares(){
-    
+function evenSquares(nums){
+    let result= nums.filter((n)=>n%2==0).map((n)=>n**2)
+    console.log(result);
 }
+
+evenSquares(nums);
+
+function sumOfOdd(nums){
+    let result=nums.filter((n)=>n%2!==0).reduce((h1,h2)=> h1+h2)
+    console.log(result);
+}
+
+sumOfOdd(nums);
+
+function firstGreaterAfterSomething(nums){
+    let result=nums.find((n)=>n>4);
+    console.log(result);
+}
+firstGreaterAfterSomething(nums);
